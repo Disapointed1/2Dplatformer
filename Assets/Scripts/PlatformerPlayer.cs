9432128799fee0a9e7d432846cbe6a8cb1081e21
Denis <denis.pelip@gmail.com>
2026-04-26 22:48:08 +0700

    all project

--- Assets/Scripts/PlatformerPlayer.cs
+++ Assets/Scripts/PlatformerPlayer.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 public class PlatformerPlayer : MonoBehaviour
 {
-    public float speed = 4.5f;
-    public float jumpForce = 12.0f;
+    private Rigidbody2D _body;
+    private Animator _animator;
+    private BoxCollider2D _box;
 
-    private Rigidbody2D body;
-    private Animator animator;
-    private BoxCollider2D box;
+    [SerializeField] private float speed = 4.5f;
+    [SerializeField] private float jumpForce = 12.0f;
     void Start()
     {
-        box = GetComponent<BoxCollider2D>();
-        body = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        _box = GetComponent<BoxCollider2D>();
+        _body = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float deltaX = Input.GetAxis("Horizontal") * speed;
-        Vector2 movement = new Vector2(deltaX, body.velocity.y);
-        body.velocity = movement;
+        Vector2 movement = new Vector2(deltaX, _body.velocity.y);
+        _body.velocity = movement;
 
-        Vector3 max = box.bounds.max;
-        Vector3 min = box.bounds.min;
+        Vector3 max = _box.bounds.max;
+        Vector3 min = _box.bounds.min;
         Vector2 corner1 = new Vector2(max.x, min.y - .1f);
         Vector2 corner2 = new Vector2(max.x, min.y - .2f);
         Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
@@ -38,10 +38,10 @@ public class PlatformerPlayer : MonoBehaviour
 
         if (grounded && Input.GetButtonDown("Jump"))
         {
-            body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            _body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
 
-        animator.SetFloat("Speed", Mathf.Abs(deltaX));
+        _animator.SetFloat("Speed", Mathf.Abs(deltaX));
 
         if (deltaX > 0)
         {
---
+++ Assets/Scripts/SawMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawMovement : MonoBehaviour
+{
+    [SerializeField] private Transform _pointA;
+    [SerializeField] private Transform _pointB;
+    [SerializeField] private float _speed = 3f;
+
+    private Vector2 _positionA;
+    private Vector2 _positionB;
+    private bool _movingToB = true;
+    void Start()
+    {
+        _positionA = _pointA.position;
+        _positionB = _pointB.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 target = _movingToB ? _positionB : _positionA;
+        transform.position = Vector2.MoveTowards(transform.position, target, _speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, target) <= 0.5f)
+        {
+           _movingToB =!_movingToB;
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent(out PlatformerPlayer player))
+        {
+            Debug.Log("HIT");
+        }
+    }
+}
